1a5064c77c67d1fc2f88b0758487f8a0ab510077
tranvuongduy2003 <tranvuongduy2003@gmail.com>
2022-05-06 15:02:48 +0700

    Done register page and add fontawesome

--- src/containers/LoginPage/LoginPage.tsx
+++ src/containers/LoginPage/LoginPage.tsx
@@ -1,5 +1,10 @@
+import Login from "components/Login/Login";
 const LoginPage = () => {
-  return <div>Login Page</div>;
+  return (
+    <div>
+      <Login></Login>
+    </div>
+  );
 };
 
 export default LoginPage;
